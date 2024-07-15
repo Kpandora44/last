4bd126c65769c6f5d5eb379990a187d3a399116f
Tatiana Koroleva <pandiracat@gmail.com>
2024-07-15 23:41:57 +0300

    Ввод первоначальных данных

--- kr/Program.cs
+++ kr/Program.cs
@@ -3,3 +3,6 @@
 //либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
+string[] array1 = new string[6] {"hello", "94", "sunshine", "inspitation", "tea","fog"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
