8f10a11ea162e6fd6f942c50f390b1073bad3288
Tatiana Koroleva <pandiracat@gmail.com>
2024-07-15 23:43:27 +0300

    Создание цикла

--- kr/Program.cs
+++ kr/Program.cs
@@ -6,3 +6,14 @@
 string[] array1 = new string[6] {"hello", "94", "sunshine", "inspitation", "tea","fog"};
 string[] array2 = new string[array1.Length];
 void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}   
